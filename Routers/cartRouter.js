a1cd249909a53901766e0cb6f057971d9e6dbfa4
Rubesh Kumar.R <rubeshkumar135@gmail.com>
2026-03-02 18:40:55 +0530

    Update project1

--- Routers/cartRouter.js
+++ Routers/cartRouter.js
@@ -1,1 +1,22 @@
-import 
+import mongoose from "mongoose";
+import {authMiddleware} from "../Middlewares/authMiddleware.js"
+import { 
+    addToCart,
+    viewCart,
+    removeFromCart,
+    updateCartItem,
+} from "../Controllers/cartController.js"
+
+const router = express.Router();
+
+
+// Add to cart 
+router.post("/add", authMiddleware, addToCart);
+// Remove to cart
+router.delete("/remove/:productId", authMiddleware, removeFromCart);
+// Update to cart
+router.put("/update/:productId", authMiddleware, updateCartItem);
+// View to cart items
+router.get("/view", authMiddleware, viewCart);
+
+export default router;
